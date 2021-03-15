9ef688a1b3b180eb4d384c35f4c56077148b87f8
Kyle Rohlfing <kylerohlfing27@gmail.com>
2021-03-15 15:04:34 -0500

    testing github push messages to discord

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,7 @@
 
 <div class="container-fluid" id="heroDiv">
     <h1 class="row">This is content!</h1>
+    <h3 class="row">Testing github push messages</h3>
 </div>
 
 <div class="container" id="testContainer">
