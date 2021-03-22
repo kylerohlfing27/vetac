6610a80e158ae8bb41765fe251f6bf6e644d4025
Arbzdv <54447074+Arbzdv@users.noreply.github.com>
2021-03-22 15:47:13 -0500

    Added cards with images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,13 +22,40 @@
 </div> -->
 
 <div class="container-fluid" id="heroDiv">
-    <h1 class="row">This is content!</h1>
-    <p>Put more content :( whore</p>
+    <h1 class="row">This Will be a stock photo slide show running through the different servicves the site offers </h1>
+    <p>ye ye </p>
 </div>
 
-<div class="container" id="testContainer">
-    <div id="testDiv" class="row">test content</div>
+<!-- https://getbootstrap.com/docs/4.4/components/card/#card-layout -->
+<div class="container">
+    <div class="card-deck" id="cardContainer">
+
+        <div class="card">
+            <img src="{{ URL::asset('/images/depression.jpg') }}" class="card-img-top" alt="ope">
+            <div class="card-body">
+                <h5 class="card-title">Mental Health Help </h5>
+                <p class="card-text">Get Help Today.</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <img src="{{ URL::asset('/images/tie_adjust.jpg') }}" class="card-img-top" id="imgShrink" alt="oopsie hehe">
+            <div class="card-body">
+                <h4 class="card-title">Professional Development</h4>
+                <p class="card-text">Develop Professional Skills </p>
+            </div>
+        </div>
+        
+        <div class="card">
+            <img src="{{ URL::asset('/images/computer_notes .jpg') }}" class="card-img-top" alt="ope">
+            <div class="card-body">
+                <h5 class="card-title">Goal Setting</h5>
+                <p class="card-text">Set your goals to change your life </p>
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <div class="container" id="newID">
 <p>new div yo</p>
