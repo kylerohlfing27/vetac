b32c15af5a60184b04f89541b0cb9c2d7b1b801b
Kyle Rohlfing <kylerohlfing27@gmail.com>
2021-03-24 13:29:09 -0500

    webpack mix updated for navbar js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/navbar.js', 'public/js/app.js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/dashboard.scss', 'public/css/app.css')
     .sass('resources/sass/feedback.scss', 'public/css/app.css')
