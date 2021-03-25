2058716f5ec06b2630d23dca0de2a71afbfdaf61
Arbzdv <54447074+Arbzdv@users.noreply.github.com>
2021-03-25 11:31:15 -0500

    added button need to work on sliding div
    added button and styled image to be inside the button

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -1,3 +1,6 @@
-function rightSlideNav() {
+function slideToTheLeft() {
     document.getElementById("sidenavbar").style.width= "200px";
+}
+function  slideToTheRight() {
+    document.getElementById("closebutton").style.width= "0";
 }
